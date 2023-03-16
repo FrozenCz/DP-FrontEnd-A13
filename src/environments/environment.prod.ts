6a64d7b2015cbea3eb20323d68c0cad7024fb2bf
FrozenCz <info@milanknop.cz>
2023-03-16 18:52:40 +0100

    oprava cesty ws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true
 };
 
-export const webSocketUrl = 'wss://46.36.39.68:3000';
-// export const webSocketUrl = 'wss://bp.milanknop.cz/rest';
+// export const webSocketUrl = 'wss://46.36.39.68:3000';
+export const webSocketUrl = 'wss://bp.milanknop.cz/rest';
 export const restIp = '/rest';
