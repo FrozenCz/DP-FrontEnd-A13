e2b62015781acd1b954ee9af3afc4a8f4356b001
FrozenCz <info@milanknop.cz>
2023-03-16 19:41:46 +0100

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
 };
 
 // export const webSocketUrl = 'wss://46.36.39.68:3000';
-export const webSocketUrl = 'wss://bp.milanknop.cz/rest/';
+// export const webSocketUrl = 'wss://bp.milanknop.cz/rest/';
+export const webSocketUrl = 'https://bp.milanknop.cz/rest/';
 export const restIp = '/rest';
